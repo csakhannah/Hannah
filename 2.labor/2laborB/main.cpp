25f88e0ec6791d3c8ccee9e0f9788a99716174e6
csakhannah <91243767+csakhannah@users.noreply.github.com>
2021-09-28 08:40:30 +0300

    2. labor feladatB- foatlok elemeinek osszege

---
+++ 2.labor/2laborB/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{ int v[100][100], n, S1=0,S2=0, i, j ;
+cout<<"n=";
+cin>>n;
+for(i=0; i<n; i++){
+    for(j=0; j<n; j++){
+        cin>>v[i][j];}
+    }
+for(i=0; i<n; i++){
+    for(j=0; j<n; j++){
+        if(i==j){
+        S1+=v[i][j];}
+        if(i+j==n-1){
+            S2+=v[i][j];}
+        }}
+        cout<<S1<<endl;
+        cout<<S2<<endl;
+        return 0;
+    }
+
+
