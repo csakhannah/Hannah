5636a3be68940013ca80bb894e2c87afe7178961
csakhannah <91243767+csakhannah@users.noreply.github.com>
2021-09-28 09:19:54 +0300

    3.labor feladat-Sf folotti elemek, Sf elemei=1, szimetrikus

---
+++ 3labor/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ int v[100][100], n, i, j ;
+cout<<"n=";
+cin>>n;
+for(i=0; i<n-1; i++){
+    for(j=i+1; j<n; j++){
+            cout<< "v["<< i <<"]["<<j<<"]=";
+        cin>>v[i][j], v[j][i]=v[i][j];}
+    }
+for(i=0; i<n; i++){
+    v[i][i]=1;}
+    for(i=0; i<n; i++){
+        for(j=0; j<n; j++){
+        cout<<v[i][j]<<" ";}
+
+        cout<<endl;
+    }
+        return 0;
+    }
