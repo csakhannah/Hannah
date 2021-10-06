7b1256735592d894d85ac1f4c7e75a8700163ba5
csakhannah <91243767+csakhannah@users.noreply.github.com>
2021-10-06 19:44:17 +0300

    fontos kodok

---
+++ lnko/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a, b;
+cout<<"a=";
+cin>>a;
+cout<<"b=";
+cin>>b;
+while(a!=b){
+if(a>b){
+    a=a-b;}
+    else{b=b-a;}
+}
+cout<<a;
+    return 0;
+}
