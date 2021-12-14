70c0123e232e08f1f7b3f98f229753864f0af94c
csakhannah <91243767+csakhannah@users.noreply.github.com>
2021-12-14 09:49:22 +0200

    6.hf probalkozas

---
+++ PARAMETEREK 3.FELADAT/main.cpp
@@ -0,0 +1,28 @@
+//Írjunk egy függvényt, amely visszatéríti egy szám tükörképét,
+//majd a főfüggvényben (main), ezt használva megvizsgáljuk, hogy a szám pallindróm-e.
+//Oldjuk meg a feladatot a következő feltételekkel:
+//a.) a vizsgálandó szám legyen globálisan deklarálva
+//b.) a vizsgálandó szám legyen lokálisan deklarálva és
+
+//Adjuk át érték szerint a függvénynek
+//Adjuk át cím szerint a függvénynek
+
+#include <iostream>
+
+using namespace std;
+void prim(int n, int x, int y){
+for(int i=n; i<n; i--){
+    if(n%2==0)
+}
+
+
+
+
+    return 0;
+
+}
+int main()
+{
+
+    return 0;
+}
