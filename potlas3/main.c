6b259098cc81cef202ceacfb0f81ef8f392d1f07
csakhannah <91243767+csakhannah@users.noreply.github.com>
2022-01-09 19:32:15 +0200

    szam tukorkepe rekurzivan

---
+++ potlas3/main.c
@@ -0,0 +1,22 @@
+//szam tukorkepe rekurzivan:
+
+
+#include<iostream>
+
+using namespace std;
+
+int tukor(long int a)
+{
+   if(a<10){return a;}
+   int x=a;
+   int h=1;
+   while(x>10){
+    x/=10;
+    h*=10;
+   }
+   return a%10*h+tukor(a/10);
+}
+ int main (){
+ cout<< tukor(123)<<endl;
+ return 0;
+ }
