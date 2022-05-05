fd40505357065ca521e98d3d5de2b812087332b2
csakhannah <91243767+csakhannah@users.noreply.github.com>
2022-05-05 07:31:58 +0300

    divprimesfeladat

---
+++ gyakorlasfelevire 3/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+int divPrim(int n){
+int oszto=2;
+int S=0;
+while(n>1){
+    int szamlalo=0;
+    while (n%oszto==0){
+        n/=oszto;
+        szamlalo++;
+    }
+if(szamlalo&2!=0){
+S+=oszto;
+}
+oszto++;}
+return S;
+}
+int main()
+{  int S=divPrim(360);
+    cout << divPrim(360)<< endl;
+    cout << divPrim(16)<< endl;
+    return 0;
+}
