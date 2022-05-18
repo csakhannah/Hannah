39472c9ba611289aa435930063fcffa41ddc0842
csakhannah <91243767+csakhannah@users.noreply.github.com>
2022-05-19 01:15:44 +0300

    csilla szavas feladata

---
+++ csilla1/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<fstream>;
+using namespace std;
+
+int main()
+{
+    int n, k, szamlalo=0;
+    string szo;
+    string szavak[100];
+    cout<<"n=";
+    cin>>n;
+    cout<<"k=";
+    cin>>k;
+    for(int i=0;i<n;i++){
+        cin>>szo;
+        if(szo[szo.size-1]=='a' || szo[szo.size-1]=='e' || szo[szo.size-1]=='i' || szo[szo.size-1]=='o' || szo[szo.size-1]=='u' ){
+            szavak[szamlalo]=szo;
+            szamlalo++;
+        }
+    }
+    cout<<endl;
+    if(szamamlo<k){
+        cout<<"nu exista";
+    }else{
+        for(int i=o;ik; i++){
+            cout<<szavak[i]<<endl;
+        }
+    }
+    return 0;
+}
